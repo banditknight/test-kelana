82fe3557225b63a90989a0e614335a7422b2fa78
Andre Ardhi <banditknight@gmail.com>
2021-10-18 17:29:51 +0700

    geoquery

--- routes/record.js
+++ routes/record.js
@@ -48,7 +48,7 @@ recordRoutes.route('/track').get(async function (_req, res) {
   const dbConnect = dbo.getDb();
 
   try {
-    let data = await dbConnect.collection('gpsrecord').find().toArray();
+    let data = await dbConnect.collection('gpstrack').find().toArray();
     return res.status(200).json({
       status: 'success',
       message: 'data berhasil ditampilkan',
