848507b91f8d0bb92d853eab1e6ea4b4d8474109
Andre Ardhi <banditknight@gmail.com>
2021-10-18 09:34:59 +0700

    history

--- routes/record.js
+++ routes/record.js
@@ -11,17 +11,31 @@ const dbo = require('../db/conn');
 recordRoutes.route('/history').get(async function (_req, res) {
   const dbConnect = dbo.getDb();
 
-  dbConnect
-    .collection('gpslog')
-    .find({})
-    .limit(50)
-    .toArray(function (err, result) {
-      if (err) {
-        res.status(400).send('Error fetching listings!');
-      } else {
-        res.json(result);
-      }
-    });
+  try {
+    let data = await dbConnect.collection('gpslog').find();
+    return res.status(200).json({
+      status: 'success',
+      message: 'data berhasil ditampilkan',
+      data
+    })    
+  } catch (error) {
+    return res.status(400).json({
+      status: 'Error!',
+      message: err.message
+    })
+  }
+
+  // dbConnect
+  //   .collection('gpslog')
+  //   .find({})
+  //   // .limit(50)
+  //   .toArray(function (err, result) {
+  //     if (err) {
+  //       res.status(400).send('Error fetching listings!');
+  //     } else {
+  //       res.json(result);
+  //     }
+  //   });
 });
 
 // This section will help you get a list of all the records.
